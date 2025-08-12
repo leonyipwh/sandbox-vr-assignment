ec3f7c27afcecb8cc330c951db99c23fd8c4acb3
Leon Yip <>
2025-08-12 19:53:11 +0800

    feat: answer check and scoring

--- packages/client/src/pages/wordle/wordle.tsx
+++ packages/client/src/pages/wordle/wordle.tsx
@@ -5,11 +5,17 @@ import { WordLength } from '@shared/config.ts';
 
 import { PinInput } from 'react-input-pin-code'
 
+type Score = 'hit' | 'present' | 'miss';
+interface GuessResult {
+  text: string;
+  score: Score
+}
+
 function Wordle() {
   const defaultValues = ['', '', '', '', ''];
 
   const [inputValues, setInputValues] = React.useState(defaultValues);
-  const [guessResult, setGuessResult] = React.useState<string[]>([]);
+  const [guessResult, setGuessResult] = React.useState<GuessResult[][]>([]);
   // console.log(GameRounds);
 
   const keyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
@@ -25,8 +31,48 @@ function Wordle() {
       return;
     }
 
-    setGuessResult(currentGuess => [...currentGuess, validInput.join('').toUpperCase()]);
+    const answer = 'HELLO';
+
+    checkAnswer(validInput, answer);
+  };
+
+  const checkAnswer = (validInput: string[], answer: string) => {
+    const result: GuessResult[]= [];
+    validInput.map((letter, index) => {
+      let score: Score = 'miss';
+      const text = letter.toUpperCase();
+      const upperCaseAnswer = answer.toUpperCase();
+
+      if (text === upperCaseAnswer[index]) {
+        score = 'hit';
+      }
+
+      if (text !== upperCaseAnswer[index] && upperCaseAnswer.includes(text)) {
+        score = 'present';
+      }
+
+      result.push({
+        text,
+        score,
+      });
+    });
+
+    setGuessResult(currentGuess => [...currentGuess, result]);
+
     setInputValues(defaultValues);
+  };
+
+  const scoreStyle = (score: Score) => {
+    switch (score) {
+      case 'hit':
+        return 'guessResult__letter--hit';
+      case 'present':
+        return 'guessResult__letter--present';
+      case 'miss':
+        return 'guessResult__letter--miss';
+      default:
+        return '';
+    }
   };
 
   return (
@@ -36,9 +82,9 @@ function Wordle() {
           guessResult.map((guess, index) => (
             <div className="guessResult__row" key={index}>
               {
-                Array.from(guess).map((letter, i) => (
-                  <div key={i}>
-                    {letter}
+                Array.from(guess).map((item, i) => (
+                  <div className={'guessResult__letter ' + scoreStyle(item.score)} key={i}>
+                    {item.text}
                   </div>
               ))}
             </div>
@@ -52,7 +98,7 @@ function Wordle() {
             onKeyDown={keyDown}
             autoFocus
             autoTab
-            onChange={(value, index, values) => setInputValues(values)}
+            onChange={(value, index, values) => setInputValues(values.map(v => v.toUpperCase()))}
           />
         </div>
 
