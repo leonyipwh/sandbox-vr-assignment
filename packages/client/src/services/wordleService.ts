443c92e404793684f2e80b68ae0c02445b16fee4
Leon Yip <>
2025-08-13 19:09:31 +0800

    feat: client wordleService

---
+++ packages/client/src/services/wordleService.ts
@@ -0,0 +1,20 @@
+import { WordleUrl } from "@shared/wordle.interface";
+
+export const startGame = async () => {
+  return fetch(`${import.meta.env.VITE_SERVER_URL}${WordleUrl.START_GAME}`);
+}
+
+export const guess = async (roomNumber:string, guess: string) => {
+  const option = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      roomNumber,
+      guess
+    }),
+  };
+
+  return fetch(`${import.meta.env.VITE_SERVER_URL}${WordleUrl.GUESS}`, option);
+}
