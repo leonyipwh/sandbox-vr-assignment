357bc225c6be390e9f9e2c5543368f36c9c36425
Leon Yip <>
2025-08-13 19:05:40 +0800

    feat: server cors

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import path from 'path';
 const app = express();
+const cors = require('cors');
+
 const port = process.env.PORT || 3000;
 
 import router from './routes/v1/router';
 
+app.use(cors());
 app.use(express.json());
 
 app.use(express.static(path.resolve(__dirname, '../client/dist')));
